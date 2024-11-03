56fb20ef4faa8d0824914669d0f6ff7686bd8512
Dmitry Bakhmutov <dmitrbah@mail.ru>
2024-11-03 13:37:19 +0300

    Completed Task #2 Cars page

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask('__name__')
 
+cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+
 @app.route('/hello_world')
 def hello_world():
     return 'Привет, мир!'
+
+@app.route('/cars')
+def cars():
+    global cars_list
+    result = ', '.join(car for car in cars_list)
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
