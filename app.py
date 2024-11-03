03283b6b0fc23e6b4889d6f6681e1bee51ae311d
Dmitry Bakhmutov <dmitrbah@mail.ru>
2024-11-03 13:43:00 +0300

    Completed Task #3 Cats page

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import random
 
 app = Flask('__name__')
 
-cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada', 'Mazda']
+cats_list = ['Корниш-рекс', 'Русская голубая', 'Шотландская вислоухая', 'Мейн-кун', 'Манчкин']
 
 @app.route('/hello_world')
 def hello_world():
@@ -12,6 +14,12 @@ def hello_world():
 def cars():
     global cars_list
     result = ', '.join(car for car in cars_list)
+    return result
+
+@app.route('/cats')
+def cats():
+    global cats_list
+    result = random.choice(cats_list)
     return result
 
 if __name__ == '__main__':
